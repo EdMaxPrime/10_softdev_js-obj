8295db0b1f9b1c74e9120ab91863da33c8d0b219
Max Z <max.zlotskiy@gmail.com>
2018-03-09 01:22:53 -0500

    fixed bug relating to event listener and the meaning of this

--- listeners.js
+++ listeners.js
@@ -2,11 +2,22 @@ var svg = document.getElementsByTagName("svg")[0];
 var cbtn = document.getElementById("clear");
 var WIDTH = parseInt(svg.getAttribute("width"));
 var HEIGHT = parseInt(svg.getAttribute("height"));
-var makeCircle = function(x, y) {
 
+//A template for circle objects
+var makeCircle = function(x, y) {
+    var obj;
+    var circleMouseEvent = function(evt) {
+        if(obj.fill == "green") {
+            obj.fill = "red";
+            obj.update(svg);
+        } else {
+            obj.remove();
+            makeCircle(Math.random() * WIDTH, Math.random() * HEIGHT).display(svg);
+        }
+    };
     var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     circle.addEventListener("click", circleMouseEvent);
-    return {
+    obj = {
         x: x,
         y: y,
         r: 10,
@@ -19,35 +30,25 @@ var makeCircle = function(x, y) {
             this.elem.setAttribute("fill", this.fill);
             svgElement.appendChild(this.elem);
         },
-          remove: function(){
-            svg.removeChild(this);
+        remove: function(){
+            this.elem.remove();
         },
         update: function(svgElement) {
             this.remove(svgElement);
             this.display(svgElement);
         }
     };
+    return obj;
 };
 
-var circleMouseEvent = function(evt) {
-    if(this.fill == "green") {
-        this.fill = "red";
-        this.update(svgElement);
-    } else {
-        this.remove();
-        makeCircle(Math.random() * WIDTH, Math.random() * HEIGHT).display(svg);
-    }
-};
-
+//Removes everything from the svg container
 var clearSVG = function() {
     while(svg.children.length > 0) {
         svg.children[0].remove();
     }
 };
 
-//MOVE THIS INTO makeCircle() so its the first line
-
-
+//This creates the initial circles
 var svgMouseEvent = function(evt) {
     if(evt.target == this) {
         makeCircle(evt.offsetX, evt.offsetY).display(svg);
